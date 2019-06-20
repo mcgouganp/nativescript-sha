a16c8a85dd1fe616a1b7d59b6e48d36bf1c7e8c8
Paul McGougan <paul@plmservices.com.au>
2019-06-20 22:20:15 +1000

    Update webpack version again

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -61,8 +61,9 @@ module.exports = env => {
 
     const tsConfigPath = resolve(projectRoot, "tsconfig.tns.json");
 
-    if (platform === "ios") {
-        entries["tns_modules/tns-core-modules/inspector_modules"] = "inspector_modules.js";
+    const areCoreModulesExternal = Array.isArray(env.externals) && env.externals.some(e => e.indexOf("tns-core-modules") > -1);
+    if (platform === "ios" && !areCoreModulesExternal) {
+        entries["tns_modules/tns-core-modules/inspector_modules"] = "inspector_modules";
     };
 
     let sourceMapFilename = nsWebpack.getSourceMapFilename(hiddenSourceMap, __dirname, dist);
